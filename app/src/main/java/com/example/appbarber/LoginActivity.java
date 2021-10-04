e86fb06b1a90dcfbf18f80ee85804bc7d98c8fca
MinhHieu259 <mhieu7252@gmail.com>
2021-10-04 07:09:29 +0700

    hieucnn

--- app/src/main/java/com/example/appbarber/LoginActivity.java
+++ app/src/main/java/com/example/appbarber/LoginActivity.java
@@ -1,14 +1,36 @@
 package com.example.appbarber;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Adapter;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class LoginActivity extends AppCompatActivity {
-
+TabLayout tabLayout;
+ViewPager viewPager;
+float v= 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        tabLayout.addTab(tabLayout.newTab().setText("Đăng nhập"));
+        tabLayout.addTab(tabLayout.newTab().setText("Đăng ký"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+         final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this, tabLayout.getTabCount());
+            viewPager.setAdapter(adapter);
+            viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+            tabLayout.setTranslationY(300);
+            tabLayout.setAlpha(v);
+
     }
 }
--- app/src/main/java/com/example/appbarber/SignupTabFragment.java
+++ app/src/main/java/com/example/appbarber/SignupTabFragment.java
@@ -1,2 +1,16 @@
-package com.example.appbarber;public class SignupTabFragment {
+package com.example.appbarber;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SignupTabFragment extends Fragment {
+
+    public View onCreaView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.sign_up_tab_fragment, container, false);
+        return root;
+    }
 }
