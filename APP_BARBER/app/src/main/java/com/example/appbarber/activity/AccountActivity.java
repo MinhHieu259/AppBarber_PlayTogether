db8580b9d7c19333faddb3060412737bcb08079d
MinhHieu259 <mhieu7252@gmail.com>
2021-10-26 21:16:30 +0700

    addmenu2

--- APP_BARBER/app/src/main/java/com/example/appbarber/activity/AccountActivity.java
+++ APP_BARBER/app/src/main/java/com/example/appbarber/activity/AccountActivity.java
@@ -1,8 +1,14 @@
 package com.example.appbarber.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.appbarber.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.taikhoan);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                , DashboardActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.lichhen:
+                        startActivity(new Intent(getApplicationContext()
+                                , CalendarActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.thongbao:
+                        startActivity(new Intent(getApplicationContext()
+                                , NotiActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.taikhoan:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- APP_BARBER/app/src/main/java/com/example/appbarber/activity/NotiActivity.java
+++ APP_BARBER/app/src/main/java/com/example/appbarber/activity/NotiActivity.java
@@ -1,8 +1,14 @@
 package com.example.appbarber.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.appbarber.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NotiActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class NotiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noti);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.thongbao);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                , DashboardActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.lichhen:
+                        startActivity(new Intent(getApplicationContext()
+                                , CalendarActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.thongbao:
+                        return true;
+                    case R.id.taikhoan:
+                        startActivity(new Intent(getApplicationContext()
+                                , AccountActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
