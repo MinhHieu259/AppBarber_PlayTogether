48343f45eb79e4ede8fc7f0a584dfea48e9530a4
MinhHieu259 <mhieu7252@gmail.com>
2021-11-03 23:17:09 +0700

    prosnd

--- APP_BARBER/app/src/main/java/com/example/appbarber/Class/SalonhelperClasss.java
+++ APP_BARBER/app/src/main/java/com/example/appbarber/Class/SalonhelperClasss.java
@@ -1,2 +1,24 @@
-package com.example.appbarber.Class;public class SalonhelperClasss {
+package com.example.appbarber.Class;
+
+public class SalonhelperClasss {
+    int  image;
+    String title, address;
+
+    public SalonhelperClasss(int image, String title, String address) {
+        this.image = image;
+        this.title = title;
+        this.address = address;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
