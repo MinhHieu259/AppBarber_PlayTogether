971a1c4fab5817dc3f3a5457503585548080dd42
Thuan Dev <ngovanthuan07@gmail.com>
2021-11-06 09:54:59 +0700

    commit

--- APP_BARBER/app/src/main/java/com/example/appbarber/Class/ThongBao.java
+++ APP_BARBER/app/src/main/java/com/example/appbarber/Class/ThongBao.java
@@ -1,2 +1,40 @@
-package com.example.appbarber.Class;public class ThongBao {
+package com.example.appbarber.Class;
+
+public class ThongBao {
+    private String tenThongBao;
+    private String noiDung;
+    private String ngayThongBao;
+    private String gioThongBao;
+
+    public String getTenThongBao() {
+        return tenThongBao;
+    }
+
+    public void setTenThongBao(String tenThongBao) {
+        this.tenThongBao = tenThongBao;
+    }
+
+    public String getNoiDung() {
+        return noiDung;
+    }
+
+    public void setNoiDung(String noiDung) {
+        this.noiDung = noiDung;
+    }
+
+    public String getNgayThongBao() {
+        return ngayThongBao;
+    }
+
+    public void setNgayThongBao(String ngayThongBao) {
+        this.ngayThongBao = ngayThongBao;
+    }
+
+    public String getGioThongBao() {
+        return gioThongBao;
+    }
+
+    public void setGioThongBao(String gioThongBao) {
+        this.gioThongBao = gioThongBao;
+    }
 }
--- APP_BARBER/app/src/main/java/com/example/appbarber/activity/NotiActivity.java
+++ APP_BARBER/app/src/main/java/com/example/appbarber/activity/NotiActivity.java
@@ -4,14 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.appbarber.Class.SalonNoti;
+import com.example.appbarber.Class.ThongBao;
 import com.example.appbarber.R;
+import com.example.appbarber.adapter.SaLonNotiAdapter;
+import com.example.appbarber.adapter.ThongBaoAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class NotiActivity extends AppCompatActivity {
+    ListView lvThongBao;
+    ArrayList<ThongBao> thongBaoArray;
+    ThongBaoAdapter thongBaoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,5 +56,32 @@ public class NotiActivity extends AppCompatActivity {
                 return false;
             }
         });
+        mapping();
+        thongBaoAdapter = new ThongBaoAdapter(NotiActivity.this, R.layout.thongbao_view, thongBaoArray);
+        lvThongBao.setAdapter(thongBaoAdapter);
+
+    }
+
+    public void mapping(){
+        lvThongBao = (ListView) findViewById(R.id.lvThongBao);
+        thongBaoArray = new ArrayList<>();
+        ThongBao thongBao = new ThongBao();
+        thongBao.setTenThongBao("Đặt lịch thành công");
+        thongBao.setNoiDung("Lịch cắt ngày 30/10/2021 đặt thành công tại 30 Shine");
+        thongBao.setNgayThongBao("29/10/2021");
+        thongBao.setGioThongBao("14:31:23");
+        ThongBao thongBao2 = new ThongBao();
+        thongBao2.setTenThongBao("Hủy lịch thành công");
+        thongBao2.setNoiDung("Hủy lịch cắt ngày 30/10/2021  thành công");
+        thongBao2.setNgayThongBao("29/10/2021");
+        thongBao2.setGioThongBao("14:31:23");
+        ThongBao thongBao3 = new ThongBao();
+        thongBao3.setTenThongBao("Mời bạn đánh giá dịch vụ");
+        thongBao3.setNoiDung("Tiệm cắt tóc 30shine");
+        thongBao3.setNgayThongBao("29/10/2021");
+        thongBao3.setGioThongBao("14:31:23");
+        thongBaoArray.add(thongBao);
+        thongBaoArray.add(thongBao2);
+        thongBaoArray.add(thongBao3);
     }
 }
