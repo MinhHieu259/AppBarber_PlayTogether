024900aa9da443f31e392c44d7934dff21df22ad
MinhHieu259 <mhieu7252@gmail.com>
2021-11-15 22:26:00 +0700

    hieu add

--- APP_BARBER/app/src/main/java/com/example/appbarber/activity/MainActivity.java
+++ APP_BARBER/app/src/main/java/com/example/appbarber/activity/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.appbarber.activity;
-import android.os.Bundle;
-
 import static android.app.ActivityOptions.makeSceneTransitionAnimation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ActivityOptions;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.Handler;
 import android.util.Pair;
 import android.view.View;
@@ -16,6 +15,8 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.appbarber.R;
 
@@ -48,15 +49,43 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                Intent intent = new Intent(MainActivity.this, OnBoardActivity.class);
                 Pair[] pairs = new Pair[2];
                 pairs[0] = new Pair<View, String>(image, "logo_image");
                 pairs[1] = new Pair<View, String>(logo, "logo_text");
                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
                     ActivityOptions options = makeSceneTransitionAnimation(MainActivity.this, pairs);
-                    startActivity(intent, options.toBundle());
+//                    startActivity(intent, options.toBundle());
                 }
             }
         },SPLASH_SCREEN);
+        // Tạm dừng app 1.5 giây
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                isFirstTime();
+            }
+        }, 1500);
+
+    }
+    private void isFirstTime() {
+        // kiểm tra chạy app lần đầu
+        SharedPreferences preferences = getApplication().getSharedPreferences("onBoard", Context.MODE_PRIVATE);
+        boolean isFirstTime = preferences.getBoolean("isFirstTime", true);
+        // set giá trị mặc định
+        if (isFirstTime){
+            SharedPreferences.Editor  editor = preferences.edit();
+            editor.putBoolean("isFirstTime", false);
+            editor.apply();
+
+            // Bắt đầu Activity Onboard
+            startActivity(new Intent(MainActivity.this, OnBoardActivity.class));
+            finish();
+        }else {
+            // Bắt đầu Auth Activity
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
     }
 }
