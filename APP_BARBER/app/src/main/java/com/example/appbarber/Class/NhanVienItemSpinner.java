80eebea14b341f67dbea75b5f97f37d90d7ac46a
MinhHieu259 <mhieu7252@gmail.com>
2022-01-10 10:56:34 +0700

    add nhanvien

--- APP_BARBER/app/src/main/java/com/example/appbarber/Class/NhanVienItemSpinner.java
+++ APP_BARBER/app/src/main/java/com/example/appbarber/Class/NhanVienItemSpinner.java
@@ -1,10 +1,22 @@
 package com.example.appbarber.Class;
 
 public class NhanVienItemSpinner {
+    private int id_nhanvien;
     private String tenNhanVien, chucVu;
 
-    public NhanVienItemSpinner(String tenNhanVien, String chucVu) {
+    public int getId_nhanvien() {
+        return id_nhanvien;
+    }
+
+    public void setId_nhanvien(int id_nhanvien) {
+        this.id_nhanvien = id_nhanvien;
+    }
+
+    public void setTenNhanVien(String tenNhanVien) {
         this.tenNhanVien = tenNhanVien;
+    }
+
+    public void setChucVu(String chucVu) {
         this.chucVu = chucVu;
     }
 
--- APP_BARBER/app/src/main/java/com/example/appbarber/activity/XacNhanActivity.java
+++ APP_BARBER/app/src/main/java/com/example/appbarber/activity/XacNhanActivity.java
@@ -1,13 +1,26 @@
 package com.example.appbarber.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.appbarber.Class.NhanVienItemSpinner;
+import com.example.appbarber.Constaint;
 import com.example.appbarber.R;
 import com.example.appbarber.adapter.NhanVienSpinnerAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -15,6 +28,7 @@ public class XacNhanActivity extends AppCompatActivity {
 private ArrayList nhanVienLists;
 private NhanVienSpinnerAdapter nhanVienSpinnerAdapter;
 private Spinner spinnerNhanVien;
+private int id_nhanvien = 0;
 TextView ngayDat;
 TextView gio;
     @Override
@@ -31,11 +45,56 @@ TextView gio;
         gio.setText(getIntent().getStringExtra("gio"));
         TextView dvChon = findViewById(R.id.dichvuDachon);
         dvChon.setText(getIntent().getStringExtra("dichvu"));
+        spinnerNhanVien.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                NhanVienItemSpinner nhanVienItemSpinner = (NhanVienItemSpinner) adapterView.getItemAtPosition(i);
+                id_nhanvien = nhanVienItemSpinner.getId_nhanvien();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     private void initNhanVienList() {
         nhanVienLists = new ArrayList<>();
 //        nhanVienLists.add(new NhanVienItemSpinner("Nguyễn Minh Hiếu", "Thợ Cắt"));
 //        nhanVienLists.add(new NhanVienItemSpinner("Ngô Văn Thuần", "Thợ Cắt"));
+        StringRequest request = new StringRequest(Request.Method.GET, Constaint.GET_NHANVIEN_BY_SALON+"/"+getIntent().getStringExtra("gio")+","+getIntent().getIntExtra("id_dichvu",0), response -> {
+
+            try {
+                JSONObject object = new JSONObject(response);
+                if (object.getBoolean("success")){
+                    JSONArray array = new JSONArray(object.getString("nhanvien"));
+
+                    for (int i = 0; i< array.length(); i++){
+                        JSONObject nhanvienObject = array.getJSONObject(i);
+                        NhanVienItemSpinner nhanVienItemSpinner = new NhanVienItemSpinner();
+                        nhanVienItemSpinner.setId_nhanvien(nhanvienObject.getInt("id"));
+                        nhanVienItemSpinner.setTenNhanVien(nhanvienObject.getString("hoTen"));
+                        nhanVienItemSpinner.setChucVu(nhanvienObject.getString("chucvu"));
+                        nhanVienLists.add(nhanVienItemSpinner);
+
+                    }
+                    if (nhanVienLists.isEmpty()){
+                        Toast.makeText(XacNhanActivity.this, "Không có nhân viên làm lúc "+getIntent().getStringExtra("gio"), Toast.LENGTH_SHORT).show();
+                    }
+                    nhanVienSpinnerAdapter = new NhanVienSpinnerAdapter(this, nhanVienLists);
+                    spinnerNhanVien.setAdapter(nhanVienSpinnerAdapter);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+            error.printStackTrace();
+        }){
+
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
     }
 }
