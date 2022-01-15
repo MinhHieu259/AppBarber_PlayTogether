68be81103263ea9217d8be170c05d0b0ff40f3e8
MinhHieu259 <mhieu7252@gmail.com>
2022-01-16 00:10:17 +0700

    add last

---
+++ APP_BARBER/app/src/main/java/com/example/appbarber/activity/ChiTietDichVuActivity.java
@@ -0,0 +1,14 @@
+package com.example.appbarber.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChiTietDichVuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_dich_vu);
+    }
+}
---
+++ APP_BARBER/app/src/main/java/com/example/appbarber/activity/DatLichDVActivity.java
@@ -0,0 +1,14 @@
+package com.example.appbarber.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatLichDVActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dat_lich_dvactivity);
+    }
+}
